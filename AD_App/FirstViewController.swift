13581d3800e67b46d6c011d3ee23bb9074fb297e
ckho <me@ckho.info>
2018-10-17 16:44:43 +0800

    Initial Commit

---
+++ AD_App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AD_App
+//
+//  Created by Chun-kit Ho on 17/10/2018.
+//  Copyright © 2018 Chun-kit Ho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
